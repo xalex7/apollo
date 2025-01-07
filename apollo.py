fc188cb259eed2887873e8511084841beed94e2a
Fady Eid <feid@apple.com>
2025-01-07 09:29:28 -0800

    Added save/load activities in txt file

--- apollo.py
+++ apollo.py
@@ -7,19 +7,17 @@ def main():
     greet_user(name, age)
     years_until_100(age)
     check_even_or_odd()
-    collect_favorite_activites()
-
-
-
+    favorite_activities = collect_favorite_activites()
+    save_activities(favorite_activities)
 
 # Custom message based on age:
 def greet_user(name, age):
     if age < 18:
-        print(f"Hi {name}, you're young!")
+        print(f"\nHi {name}, you're young!")
     elif 18 <= age < 60:
-        print(f"Hi {name}, you're an adult!")
+        print(f"\nHi {name}, you're an adult!")
     else:
-        print(f"Hi {name}, You're at your golden age!")
+        print(f"\nHi {name}, You're at your golden age!")
 
 # Years until 100:
 def years_until_100(age):
@@ -33,9 +31,9 @@ def check_even_or_odd():
 
     # Analyzing a number:
     if number % 2 == 0:
-        print(f"The number {number} is even.")
+        print(f"The number {number} is even.\n")
     else:
-        print(f"The number {number} is odd.")
+        print(f"The number {number} is odd.\n")
 
 # Collect favorite activities:
 def collect_favorite_activites():
@@ -59,7 +57,7 @@ def collect_favorite_activites():
         print(f"- {activity}")
 
     # Option to remove an activity:
-    wantToRemove = input("\nWould you like to remove any of the activites? <Yes/No>")
+    wantToRemove = input("\nWould you like to remove any of the activites? <Yes/No> ")
     if wantToRemove.lower() == "yes":
         removeActivity = input("Which activity would you like to remove? ")
         if removeActivity in activities:
@@ -73,6 +71,20 @@ def collect_favorite_activites():
             print(f"- {activity}")
     else:
         print("\nThank You!\n")
+    
+    return activities
+    
+def save_activities(activities):
+    with open("activities.txt", "w") as file:
+        for activity in activities:
+            file.write(activity + "\n")
 
+def load_activities():
+    try:
+        with open ("activities.txt", "r") as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        return []
+    
 if __name__ == "__main__":
     main()
